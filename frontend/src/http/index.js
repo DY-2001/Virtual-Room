8d800475ba763b0136a49cbd9fa6d07cf4421803
DY-2001 <dushyant.y20@iiits.in>
2024-07-03 00:33:55 +0530

    docker work

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -25,7 +25,7 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
     if (
-      error.response.status === 401 &&
+      error.response?.status === 401 &&
       originalRequest &&
       !originalRequest._isRetry
     ) {
